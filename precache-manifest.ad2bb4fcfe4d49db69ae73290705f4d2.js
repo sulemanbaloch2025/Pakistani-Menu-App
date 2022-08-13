e69f3259e75a61910bcc061fcec66ab467149ffc
Suleman Baloch <sulemanbaloch@Sulemans-MacBook-Pro.local>
2022-08-13 19:45:36 -0400

    Updates

---
+++ precache-manifest.ad2bb4fcfe4d49db69ae73290705f4d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf9518613d2262f6554a82a86447526",
+    "url": "/Pakistani-Menu-App/index.html"
+  },
+  {
+    "revision": "29f3d34b2292123bef63",
+    "url": "/Pakistani-Menu-App/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/Pakistani-Menu-App/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pakistani-Menu-App/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f3d34b2292123bef63",
+    "url": "/Pakistani-Menu-App/static/js/main.c4d2279d.chunk.js"
+  },
+  {
+    "revision": "dc207c33fbf7a9724f30",
+    "url": "/Pakistani-Menu-App/static/js/runtime-main.3c51cf4d.js"
+  }
+]);
